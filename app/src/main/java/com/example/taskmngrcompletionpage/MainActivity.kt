4fbbd76d817973a754a1f9fcf38b8c4b825e0d94
bishalpal <bishaldg47@gmail.com>
2024-01-14 10:37:39 +0530

    Added resources and Composable functions for UI components.

--- app/src/main/java/com/example/taskmngrcompletionpage/MainActivity.kt
+++ app/src/main/java/com/example/taskmngrcompletionpage/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.taskmngrcompletionpage
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmngrcompletionpage.ui.theme.TaskMngrCompletionPageTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting()
+                    TaskCompletion()
                 }
             }
         }
@@ -30,17 +42,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(modifier: Modifier = Modifier) {
-    Text(
-        text = "",
-        modifier = modifier
-    )
+fun TaskCompletion(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_task_completed),
+            contentDescription = null,
+        )
+        Text(
+            text = stringResource(R.string.message),
+            style = TextStyle(
+                fontWeight = FontWeight.Bold
+            ),
+            modifier = Modifier
+                .padding(
+                    top = 24.dp,
+                    bottom = 8.dp
+                )
+        )
+        Text(
+            text = stringResource(id = R.string.msg_attribute),
+            style = TextStyle(
+                fontSize = 16.sp
+            )
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TaskCompletionPreview() {
     TaskMngrCompletionPageTheme {
-        Greeting()
+        TaskCompletion()
     }
 }
